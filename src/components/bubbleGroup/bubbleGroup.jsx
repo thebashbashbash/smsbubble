cd058d8bd3da4ea74843ec4e0f3791275046480b
Bashar Mengana <bashar@Bashars-MBP.lan>
2019-03-16 16:17:09 +0100

    fixed bubblegroup

--- src/components/bubbleGroup/bubbleGroup.jsx
+++ src/components/bubbleGroup/bubbleGroup.jsx
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 import Bubble from '../bubble/bubble';
 
 import classesBubbleGroup from './bubbleGroup.module.css';
-import classesBubble from '../bubble/bubble.module.css';
 
 const bubbleGroup = ({ color, children }) => (
   <div className={classesBubbleGroup.bubbleGroup}>
-    {React.Children.map(children, (child, index, array) => React.cloneElement(child, {
+    {React.Children.map(children, (child, index) => React.cloneElement(child, {
       color,
-      tail: array.length - 1 === index ? classesBubble.Tail : [],
+      tail: children.length - 1 === index,
     }))}
   </div>
 );
