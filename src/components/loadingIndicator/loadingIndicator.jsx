1d2bc0d37987193fbb6af2b16eb8309265d43e43
Bashar Mengana <bashar@Bashars-MacBook-Pro.local>
2019-03-17 17:12:50 +0100

    fixed loading indicator color

--- src/components/loadingIndicator/loadingIndicator.jsx
+++ src/components/loadingIndicator/loadingIndicator.jsx
@@ -3,12 +3,28 @@ import PropTypes from 'prop-types';
 
 import classes from './loadingIndicator.module.css';
 
-const loadingIndicator = () => (
+const loadingIndicator = ({ color }) => (
   <div className={classes.Loading}>
-    <div className={[classes.Dot, classes.One].join(' ')} />
-    <div className={[classes.Dot, classes.Two].join(' ')} />
-    <div className={[classes.Dot, classes.Three].join(' ')} />
+    <div
+      className={[color === 'blue' ? classes.Blue : classes.Gray, classes.Dot, classes.One].join(
+        ' ',
+      )}
+    />
+    <div
+      className={[color === 'blue' ? classes.Blue : classes.Gray, classes.Dot, classes.Two].join(
+        ' ',
+      )}
+    />
+    <div
+      className={[color === 'blue' ? classes.Blue : classes.Gray, classes.Dot, classes.Three].join(
+        ' ',
+      )}
+    />
   </div>
 );
+
+loadingIndicator.propTypes = {
+  color: PropTypes.oneOf(['gray', 'blue']).isRequired,
+};
 
 export default loadingIndicator;
