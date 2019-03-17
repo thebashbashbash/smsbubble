15f111a0df177ca2815650b321fc7880435005bc
Bashar Mengana <bashar@Bashars-MBP.lan>
2019-03-17 12:11:45 +0100

    added trail

--- src/components/bubble/bubble.jsx
+++ src/components/bubble/bubble.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import classesBubble from './bubble.module.css';
-import classesBubbleLoading from './bubbleLoading.module.css';
 
-const bubble = ({
-  color, loading, tail, children,
-}) => (
+const bubble = ({ color, tail, children }) => (
   <div className={color === 'blue' ? classesBubble.Blue : classesBubble.Gray}>
-    <div className={[classesBubble.Bubble, tail ? classesBubble.Tail : []].join(' ')}>
+    <div className={[classesBubble.Bubble, tail ? classesBubble.Trail : []].join(' ')}>
       {children}
     </div>
   </div>
@@ -17,14 +14,14 @@ const bubble = ({
 bubble.propTypes = {
   color: PropTypes.oneOf(['gray', 'blue']),
   tail: PropTypes.bool,
-  loading: PropTypes.bool,
+  // loading: PropTypes.bool,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 };
 
 bubble.defaultProps = {
   color: 'blue',
   tail: false,
-  loading: false,
+  // loading: false,
   children: <div />,
 };
 
