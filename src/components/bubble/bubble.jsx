818ddb7a6c980a49d9fdd199dec9bdb95fc8656c
Bashar Mengana <bashar@Bashars-MBP.lan>
2019-03-17 12:55:10 +0100

    first version of bubble done

--- src/components/bubble/bubble.jsx
+++ src/components/bubble/bubble.jsx
@@ -2,8 +2,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import LoadingIndicator from '../loadingIndicator/loadingIndicator';
-
 import classes from './bubble.module.css';
 
 const bubble = ({ color, tail, children }) => (
