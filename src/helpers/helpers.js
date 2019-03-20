5ad7900d2d335f3173ba57723cd3c00b8193eade
Bashar Mengana <bashar@Bashars-MacBook-Pro.local>
2019-03-20 01:58:45 +0100

    fixed typing speed

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -2,7 +2,7 @@
 export const combineClasses = (...args) => [...args].join(' ');
 
 export const computeTypingSpeed = (content) => {
-  const typingSpeed = 2;
+  const typingSpeed = 4;
   return Math.round(typingSpeed * (1 / 10) * 1000 * parseInt(content.split(' ').length, 10));
 };
 
