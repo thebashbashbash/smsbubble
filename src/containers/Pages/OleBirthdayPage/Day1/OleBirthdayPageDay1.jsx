80ee16a048b1dd5af20eaaa53e7c61683f174337
Bashar Mengana <bashar@Bashars-MacBook-Pro.local>
2019-03-23 11:17:59 +0100

    updated

--- src/containers/Pages/OleBirthdayPage/Day1/OleBirthdayPageDay1.jsx
+++ src/containers/Pages/OleBirthdayPage/Day1/OleBirthdayPageDay1.jsx
@@ -12,65 +12,65 @@ const oleBirthdayPageDay1 = () => (
   <MessageConversation>
     <MessageContainer subject={Bashar}>
       <Message>Hej Habibi❤️!</Message>
-      <Message>Bra jobbat med gårdagens quiz🏅! Bara två fel!!</Message>
+      <Message>Bra jobbat igår! Du var nära, men inte riktigt helt rätt ute!🎁</Message>
+      <Message>🤠</Message>
+      <Message>Du får en ny chans idag!</Message>
     </MessageContainer>
 
     <MessageContainer subject={Ole}>
-      <Message>HAJ IGEN HABIBI!</Message>
-      <Message>Taaaaack 😅!</Message>
-      <Message>Egentligen hade jag bara ett fel, men men 🤓</Message>
-      <Message>Blir det fler frågor idag?🤠</Message>
-      <Message>miiiip!</Message>
+      <Message>HAI HAI HAI!</Message>
+      <Message>ååååå var jag nära alltså?🤠</Message>
+      <Message>hur nära? 🙈</Message>
+      <Message>får jag fler ledtrådar idag? 🏝☠️</Message>
     </MessageContainer>
 
     <MessageContainer subject={Bashar}>
+      <Message>Jaaa så klart du får fler ledtrådar idag, MEN, med en twist igen 🎢!</Message>
       <Message>
-        Jaaa men det är så klart habibi! Det blir fler frågor, MEN, med en twist! 🎢
+        Du får nu 2 frågor och för varje rätt fråga får du ställa en fråga till mig ... 🤓
       </Message>
-      <Message>
-        Du kan redan nu få din gåva OM du lyckas gissa rätt på var den finns🎁 ... MEN, du får en
-        chans att gissa och jag säger till om svaret är rätt! 🔮
-      </Message>
-    </MessageContainer>
-
-    <MessageContainer subject={Ole}>
-      <Message>😍😍😍</Message>
-      <Message>EEEEEK!</Message>
-      <Message>Menar du att jag kan få min gåva redan idag?! 🤩</Message>
-    </MessageContainer>
-
-    <MessageContainer subject={Bashar}>
-      <Message>Jaa, riktig! 🇳🇴</Message>
-      <Message>Om du gissar rätt är din present din, redan idag!</Message>
-      <Message>Redo??!</Message>
     </MessageContainer>
 
     <MessageContainer subject={Ole}>
       <Message>EEEEEK!</Message>
-      <Message>JAAA!</Message>
-      <Message>Jag är reda! Give it to me, låt skattjakten börja!🏝☠️</Message>
+      <Message>du är så morsom du! 🤩 kan jag ställa frågor som hjälper mig hitta gåvan?</Message>
     </MessageContainer>
 
     <MessageContainer subject={Bashar}>
-      <Message>1️⃣: Du tittar ut genom den franska balkongen i mörkret, på snön!</Message>
-      <Message>2️⃣: Du tittar stolt mot mig med din kreation!</Message>
-      <Message>3️⃣: En vision som påminner dig om en kärlek som blommar!</Message>
-      <Message>4️⃣: Du fångar mig på språng!</Message>
+      <Message>Jaa, helt riktig! MEN, det finns 2 regel dock 🚦!</Message>
+      <Message>
+        1 innan du börjar leta måste du har ställt alla dina frågor (1 fråga per rätt fråga du
+        svarar på!)😇
+      </Message>
+      <Message>
+        2 du får bara gissa/leta på en plats, om du inte lyckas så fortsätter vi imorgon!🗺
+      </Message>
     </MessageContainer>
 
     <MessageContainer subject={Ole}>
-      <Message>MEN VA? Vad svårt!🤓</Message>
-      <Message>😓</Message>
+      <Message>OK 😄</Message>
+      <Message>jag är redo! 😆😆😆 låt skattjakten börja!🏝☠️</Message>
     </MessageContainer>
 
     <MessageContainer subject={Bashar}>
       <Message>
-        HAHA!🤪 Det finns inga svar på varje enskild fråga, men tillsammans är dom en ledtråd som
-        talar om var gåvan finns!🏝
+        1️⃣: Det var en egenskap hos mig du tyckte var rolig / irriterande redan första
+        telefonsamtalet, vad var det?
       </Message>
       <Message>
-        Jag älskar dig, men du får bara en chans att gissa på vara gåvan finns 😅... om du inte
-        lyckas får du vänta till imorgon!🔮
+        2️⃣: När jag gjorde din alla hjärtans dag present gjorde jag illa mig, hur och var?
+      </Message>
+    </MessageContainer>
+
+    <MessageContainer subject={Ole}>
+      <Message>AHHH 😁 Ok får jag börja gissa?</Message>
+      <Message>🤠</Message>
+    </MessageContainer>
+
+    <MessageContainer subject={Bashar}>
+      <Message>HAHA!🤪 Jaa, du får börja gissa nu!</Message>
+      <Message>
+        Glöm inte att jag älskar dig... om du inte lyckas får du vänta till imorgon!🔮
       </Message>
       <Message>❤️</Message>
     </MessageContainer>
