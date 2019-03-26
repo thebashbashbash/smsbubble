1cd0543a39cdd01a47992d0372530ab0a35a4fcd
Bashar Mengana <bashar@Bashars-MacBook-Pro.local>
2019-03-26 10:39:36 +0100

    removed unused file

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import ResumePage from './containers/Pages/ResumePage/ResumePage';
-// import OleBirthdayPageDay1 from './containers/Pages/OleBirthdayPage/Day1/OleBirthdayPageDay1';
 
 const main = () => (
   <BrowserRouter>
-    <Route path="/" component={ResumePage} />
+    <Route path="/" exact component={ResumePage} />
   </BrowserRouter>
 );
 
