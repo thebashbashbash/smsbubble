4e734233d5ba4e03ad5eb91b93ad39e3ff4a3259
Bashar Mengana <bashar@Bashars-MacBook-Pro.local>
2019-03-26 20:58:10 +0100

    fixed loading indicator

--- src/containers/MessageConversation/helpers.js
+++ src/containers/MessageConversation/helpers.js
@@ -11,8 +11,11 @@ export const computeMessageDeley = (
 ) => (children instanceof Array
   ? children
     .map(child => child.props.children)
-    .map(element => element.map(subElement => subElement.props.children))
-    .map(element => element.map(() => getRandomInt(messageDelayMin, messageDelayMax)))
+    // eslint-disable-next-line max-len
+    .map(element => (element instanceof Array ? element.map(subElement => subElement.props.children) : element))
+    .map(element => (element instanceof Array
+      ? element.map(() => getRandomInt(messageDelayMin, messageDelayMax))
+      : getRandomInt(messageDelayMin, messageDelayMax)))
   : children.props.children instanceof Array
     ? children.props.children.map(() => getRandomInt(messageDelayMin, messageDelayMax))
     : getRandomInt(messageDelayMin, messageDelayMax));
@@ -25,9 +28,13 @@ export const computeConversationStartDeley = (
 export const computeMessageContainerTimeDeley = children => (children instanceof Array
   ? children
     .map(child => child.props.children)
-    .map(element => element.map(subElement => subElement.props.children))
-    .map(element => element.map(subElement => computeTypingSpeed(subElement)))
-    .map(element => element.reduce((a, b) => a + b, 0))
+    .map(element => (element instanceof Array
+      ? element.map(subElement => subElement.props.children)
+      : element.props.children))
+    .map(element => (element instanceof Array
+      ? element.map(subElement => computeTypingSpeed(subElement))
+      : computeTypingSpeed(element)))
+    .map(element => (element instanceof Array ? element.reduce((a, b) => a + b, 0) : element))
     .reduce(
       (previous, current, index) => {
         previous.push((previous[index] || 0) + current);
@@ -39,7 +46,7 @@ export const computeMessageContainerTimeDeley = children => (children instanceof
   : [0]);
 
 export const computeAccumulatedMessageDeley = (messageDelay, index) => messageDelay
-  .map(element => element.reduce((a, b) => a + b, 0))
+  .map(element => (element instanceof Array ? element.reduce((a, b) => a + b, 0) : element))
   .reduce(
     (previous, current, subIndex) => {
       previous.push((previous[subIndex] || 0) + current);
