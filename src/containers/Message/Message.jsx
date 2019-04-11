343fce07e23c8a8a5e9a8cc6e4b6062ba85b9cad
Bashar Mengana <mengana@kth.se>
2019-04-11 18:53:11 +0200

    d

--- src/containers/Message/Message.jsx
+++ src/containers/Message/Message.jsx
@@ -44,7 +44,7 @@ const MessageBubble = posed.div({
     scale: 1,
     transition: () => ({
       type: 'keyframes',
-      values: [1, 1.005, 1],
+      values: [1, 1.01, 1],
       duration: 300,
     }),
   },
