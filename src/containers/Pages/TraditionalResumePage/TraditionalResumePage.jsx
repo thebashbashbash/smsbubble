db656bea00a4452feb9195f6cda06825d8537649
Bashar Mengana <mengana@kth.se>
2019-06-12 16:34:37 +0200

    fixed work

--- src/containers/Pages/TraditionalResumePage/TraditionalResumePage.jsx
+++ src/containers/Pages/TraditionalResumePage/TraditionalResumePage.jsx
@@ -17,8 +17,42 @@ const JSONresume = {
     birtday: Number('19880126'),
     spokenLanguages: ['swedish', 'english', 'arabic', 'german'],
   },
-  education: 'KTH',
-  workExperience: 'Scania',
+  education: {
+    school_eng: 'Royal Institute of Technology',
+    school_swe: 'Kungliga Tekniska Högskolan',
+    degree_eng: 'Master of science',
+    degree_swe: 'Civilingenjör',
+    city: 'Stockholm',
+    durationInYears: Number(5),
+    completed: true,
+  },
+  workExperience: {
+    fullStackDeveloper: {
+      employer: 'Scania',
+      title: 'Full stack developer',
+      fromTo: '2017/10 - Now',
+      details: 'bla bla bla',
+    },
+    serviceDesigner: {
+      employer: 'Scania',
+      title: 'Service designer',
+      fromTo: '2016/11 - 2017/07',
+      details: 'bla bla bla',
+    },
+    productOwner: {
+      employer: 'Scania',
+      title: 'Product owner',
+      fromTo: '2015/01 - 2016/11',
+      details: 'bla bla bla',
+    },
+    trainee: {
+      employer: 'Scania',
+      title: 'Trainee',
+      fromTo: '2013/08 - 2014/12',
+      details: 'bla bla bla',
+    },
+  },
+
   skills: 'Coding',
   interests: '',
 };
@@ -27,7 +61,7 @@ const traditionalResumePage = () => (
   <div>
     <ReactJson
       iconStyle="triangle"
-      theme="grayscale:inverted"
+      theme="summerfruit:inverted"
       displayDataTypes={false}
       displayObjectSize={false}
       collapsed={2}
