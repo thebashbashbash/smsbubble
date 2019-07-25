23f6aab0f30e30f8e5bd622bd60ebdd49965da71
Bashar Mengana <mengana@kth.se>
2019-07-25 12:29:27 +0200

    fixed color palette

--- src/containers/Pages/TraditionalResumePage/TraditionalResumePage.jsx
+++ src/containers/Pages/TraditionalResumePage/TraditionalResumePage.jsx
@@ -120,15 +120,15 @@ const traditionalResumePage = () => (
         base04: '#444', // Dark Foreground (Used for status bars)
         base05: '#444', // Default Foreground, Caret, Delimiters, Operators
         base06: '#444', // Light Foreground (Not often used)
-        base07: 'rgb(20,20,20)', // Light Background (Not often used)
+        base07: 'rgb(15,15,15)', // Light Background (Not often used)
         base08: '#444', // Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-        base09: 'rgb(105,105,105)', // Integers, Boolean, Constants, XML Attributes, Markup Link Url
+        base09: 'rgb(110,110,110)', // Integers, Boolean, Constants, XML Attributes, Markup Link Url
         base0A: '#444', // Classes, Markup Bold, Search Text Background
-        base0B: 'rgb(4,165,246)', // Strings, Inherited Class, Markup Code, Diff Inserted
+        base0B: 'rgb(0,168,107)', // Strings, Inherited Class, Markup Code, Diff Inserted
         base0C: '#444', // Support, Regular Expressions, Escape Characters, Markup Quotes
-        base0D: 'rgb(120,120,120)', // Functions, Methods, Attribute IDs, Headings
-        base0E: 'rgb(150,150,150)', // Keywords, Storage, Selector, Markup Italic, Diff Changed
-        base0F: 'rgb(4,165,246)', // Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+        base0D: 'rgb(110,110,110)', // Functions, Methods, Attribute IDs, Headings
+        base0E: 'rgb(155,155,155)', // Keywords, Storage, Selector, Markup Italic, Diff Changed
+        base0F: 'rgb(0,168,107)', // Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
       }}
       displayDataTypes={false}
       displayObjectSize={false}
