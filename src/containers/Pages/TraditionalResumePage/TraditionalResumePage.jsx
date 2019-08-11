6d75b55078a83223ed99f5cfea75d6d3d4ff964b
Bashar Mengana <mengana@kth.se>
2019-08-11 20:25:56 +0200

    stub for traditional resume page

--- src/containers/Pages/TraditionalResumePage/TraditionalResumePage.jsx
+++ src/containers/Pages/TraditionalResumePage/TraditionalResumePage.jsx
@@ -2,13 +2,19 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 
-// import DynamicResumeInJson from '../../../assets/resumeInJson';
-
-// import classes from './NerdyResumePage.module.css';
+import classes from './TraditionalResumePage.module.css';
 
 ReactGA.initialize('UA-139986234-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-const traditionalResumePage = () => <div>traditionalResumePage</div>;
+const traditionalResumePage = () => (
+  <div className={classes.Container}>
+    <h2>about me</h2>
+    <h2>education</h2>
+    <h2>work experience</h2>
+    <h2>skills</h2>
+    <h2>interests</h2>
+  </div>
+);
 
 export default traditionalResumePage;
