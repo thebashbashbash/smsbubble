5e4df7b80e33a01b154b25a1ebd72f186bdafcf3
Bashar Mengana <mengana@kth.se>
2019-08-11 20:19:17 +0200

    start building traditional resume

--- src/containers/Pages/PickResumePage/PickResumePage.jsx
+++ src/containers/Pages/PickResumePage/PickResumePage.jsx
@@ -2,14 +2,22 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 
+import { AwesomeButton } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
+
 import classes from './PickResumePage.module.css';
 
 ReactGA.initialize('UA-139986234-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 const pickResumePage = () => (
-  <div className={classes.Container}>
-    <div>hej</div>
+  <div>
+    <div className={classes.Heading}>What kind of resume would you like to see?</div>
+    <div className={classes.ButtonContainer}>
+      <AwesomeButton type="primary">Traditional</AwesomeButton>
+      <AwesomeButton type="primary">Nerdy</AwesomeButton>
+      <AwesomeButton type="primary">Fun</AwesomeButton>
+    </div>
   </div>
 );
 
