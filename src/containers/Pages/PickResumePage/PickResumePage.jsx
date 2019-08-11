492b126bf62802be7ade8775fb891025738d2732
Bashar Mengana <mengana@kth.se>
2019-08-11 19:46:46 +0200

    uninstalled react awesome button, update reactscri

--- src/containers/Pages/PickResumePage/PickResumePage.jsx
+++ src/containers/Pages/PickResumePage/PickResumePage.jsx
@@ -2,8 +2,6 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 
-import AwesomeButton from 'react-native-really-awesome-button';
-
 import classes from './PickResumePage.module.css';
 
 ReactGA.initialize('UA-139986234-1');
@@ -12,7 +10,6 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 const pickResumePage = () => (
   <div className={classes.Container}>
     <div>hej</div>
-    <AwesomeButton>Text</AwesomeButton>;
   </div>
 );
 
