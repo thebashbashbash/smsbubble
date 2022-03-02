1115e88716ddc18b519ea1c4b27d5084d41a2038
Bashar Mengana <bashar.mengana@klarna.com>
2022-03-02 19:10:43 +0100

    added helmet and meta data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet";
+import Main from "./main";
 
-import Main from './main';
-
-const App = () => <Main />;
+const App = () => (
+  <div>
+    <Helmet>
+      <title>Bashar Mengana</title>
+      <meta
+        name="description"
+        content="Do you want to get to know me - Bashar Mengana - better? Check out this website!"
+      />
+      <meta name="theme-color" content="#008f68" />
+      <meta property="og:image" content="%PUBLIC_URL%/Preview.png" />
+    </Helmet>
+    <Main />
+  </div>
+);
 
 export default App;
