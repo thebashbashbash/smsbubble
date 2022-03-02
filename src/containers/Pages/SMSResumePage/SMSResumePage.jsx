dd62e71d26d81fb8a016358840e8b5c588ba4d0d
Bashar Mengana <bashar.mengana@klarna.com>
2022-03-02 20:40:43 +0100

    removed not needed empty

--- src/containers/Pages/SMSResumePage/SMSResumePage.jsx
+++ src/containers/Pages/SMSResumePage/SMSResumePage.jsx
@@ -100,7 +100,6 @@ export class ConversationResumePage extends Component {
                     color="gray"
                     style={{ verticalAlign: "top" }}
                   >
-                    {" "}
                     <ForwardArrow />
                   </font>
                 </div>
