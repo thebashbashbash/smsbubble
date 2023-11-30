f6123d1095c3ab1444305a2469e2e78baf8c156c
Bashar Mengana <bashar.mengana@klarna.com>
2023-11-30 17:51:34 +0000

    temporary removal

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-// eslint-disable-next-line import/no-named-as-default
-import SMSResumePage from "./containers/Pages/SMSResumePage/SMSResumePage";
-// import ResumePage from './containers/Pages/ResumePage/ResumePage';
-import ProjectsPage from "./containers/Pages/ProjectsPage/ProjectsPage";
-// import ILoveYou from './containers/Pages/Funny/ILoveYou/ILoveYou';
-import ErrorPage from "./containers/Pages/ErrorPage/ErrorPage";
+// // eslint-disable-next-line import/no-named-as-default
+// import SMSResumePage from "./containers/Pages/SMSResumePage/SMSResumePage";
+// // import ResumePage from './containers/Pages/ResumePage/ResumePage';
+// import ProjectsPage from "./containers/Pages/ProjectsPage/ProjectsPage";
+// // import ILoveYou from './containers/Pages/Funny/ILoveYou/ILoveYou';
+// import ErrorPage from "./containers/Pages/ErrorPage/ErrorPage";
 
-const main = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={SMSResumePage} />
-      {/* <Route path="/resume" exact component={ResumePage} /> */}
-      {/* <Route path="/smsresume" exact component={SMSResumePage} /> */}
-      <Route path="/projects" exact component={ProjectsPage} />
-      {/* <Route path="/iloveyou" exact component={ILoveYou} />  */}
-      <Route component={ErrorPage} />
-    </Switch>
-  </BrowserRouter>
-);
+const main = () => <></>;
 
 export default main;
+
+//<BrowserRouter>
+//<Switch>
+//  <Route path="/" exact component={SMSResumePage} />
+//  {/* <Route path="/resume" exact component={ResumePage} /> */}
+//  {/* <Route path="/smsresume" exact component={SMSResumePage} /> */}
+//  <Route path="/projects" exact component={ProjectsPage} />
+//  {/* <Route path="/iloveyou" exact component={ILoveYou} />  */}
+//  <Route component={ErrorPage} />
+//</Switch>
+//</BrowserRouter>
